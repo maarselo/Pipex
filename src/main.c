001d90443dc3bfad1acd3d3832972d923956fdbd
maarselo <marce@decaos.com>
2025-05-28 19:56:27 +0200

    Add without test

--- src/main.c
+++ src/main.c
@@ -26,16 +26,12 @@ static void	ft_child_process(int *fd, char **argv, char **envp)
 	close(filein);
 	dup2(fd[1], STDOUT_FILENO);
 	close(fd[1]);
-	char *msg = "Hola desde el hijo\n";
-	write(STDOUT_FILENO, msg, strlen(msg));
-	exit(0);
+	
 }
 
 static void	ft_parent_process(int *fd, char **argv, char **envp)
 {
-	(void)envp;
 	int	fileout;
-	char buffer[1000];
 
 	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fileout == -1)
@@ -45,12 +41,7 @@ static void	ft_parent_process(int *fd, char **argv, char **envp)
 	close(fileout);
 	dup2(fd[0], STDIN_FILENO);
 	close(fd[0]);
-	ssize_t bytes = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
-	if (bytes > 0)
-	{
-		buffer[bytes] = '\0';
-		printf("Padre recibió: %s", buffer);
-	}
+
 }
 
 int	main(int argc, char **argv, char **envp)
