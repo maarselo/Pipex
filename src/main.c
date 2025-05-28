acf731f4866d378944959d89215bd7d1b5b51436
maarselo <marce@decaos.com>
2025-05-28 19:34:37 +0200

    Test for main, for check if it works, and works, put with read an example in the file and put

--- src/main.c
+++ src/main.c
@@ -11,15 +11,46 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <string.h>
 
-static void	ft_child_process(char **argv, int *fd, char *envp) {
-	int file;
-	
-	file = open(argv[1], O_RDONLY);
-	if (file != -1)
+static void	ft_child_process(int *fd, char **argv, char **envp)
+{
+	(void)envp;
+	int	filein;
+
+	filein = open(argv[1], O_RDONLY);
+	if (filein == -1)
 		ft_error();
-	
+	close(fd[0]);
+	dup2(filein, STDIN_FILENO);
+	close(filein);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[1]);
+	char *msg = "Hola desde el hijo\n";
+	write(STDOUT_FILENO, msg, strlen(msg));
+	exit(0);
+}
 
+static void	ft_parent_process(int *fd, char **argv, char **envp)
+{
+	(void)envp;
+	int	fileout;
+	char buffer[1000];
+
+	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fileout == -1)
+		ft_error();
+	close(fd[1]);
+	dup2(fileout, STDOUT_FILENO);
+	close(fileout);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	ssize_t bytes = read(STDIN_FILENO, buffer, sizeof(buffer) - 1);
+	if (bytes > 0)
+	{
+		buffer[bytes] = '\0';
+		printf("Padre recibió: %s", buffer);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -35,8 +66,9 @@ int	main(int argc, char **argv, char **envp)
 		if (pid == -1)
 			ft_error();
 		if (pid == 0)
-			ft_child_process(argv, &fd, envp);
+			ft_child_process(fd, argv, envp);
 		wait(NULL);
+		ft_parent_process(fd, argv, envp);
 	}
 	else
 		ft_putstr_fd("Error: Bad arguments.", 2);
