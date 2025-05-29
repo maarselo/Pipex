1049fb759d4fdb821567b7439feaf1af0f05e505
maarselo <marce@decaos.com>
2025-05-29 19:42:05 +0200

    need to modiifed the form to open files

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -14,12 +14,27 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	int	i;
+	int	fdin;
+	int fdout;
+
 	if (argc >= 5)
 	{
 		if (!ft_strncmp(argv[1], "here_doc", 8))
 		{
+			i = 3;
+			fdout = ft_open_file(argv[argc - 1], HEREDOC_OUTFILE);
 			ft_heredoc(argc, argv);
 		}
+		else
+		{
+			i = 2;
+			fdin = open(argv[1], O_RDONLY);
+			if (open)
+			dup2(fdin, STDIN_FILENO);
+		}
+		while (i < argc -2)
+			ft_child_process(argv[i++], envp);
 	}
 
 }
