82aa3a0a03443e82365dc61c49cf870631f404c6
maarselo <marce@decaos.com>
2025-05-30 12:32:49 +0200

    Actualizate for make bonus

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -15,26 +15,28 @@
 int	main(int argc, char **argv, char **envp)
 {
 	int	i;
-	int	fdin;
 	int fdout;
 
 	if (argc >= 5)
 	{
-		if (!ft_strncmp(argv[1], "here_doc", 8))
+		if (ft_strncmp(argv[1], "here_doc", 8))
 		{
-			i = 3;
-			fdout = ft_open_file(argv[argc - 1], HEREDOC_OUTFILE);
-			ft_heredoc(argc, argv);
+			i = 2;
+			fdout = ft_open_file(argv[argc - 1], OUTFILE);
+			ft_open_file(argv[1], INFILE); //modificate the stdin
 		}
 		else
 		{
-			i = 2;
-			fdin = open(argv[1], O_RDONLY);
-			if (open)
-			dup2(fdin, STDIN_FILENO);
+			i = 3;
+			fdout = ft_open_file(argv[argc - 1], HEREDOC_OUTFILE);
+			ft_heredoc(argv); //modificate the stdin
 		}
-		while (i < argc -2)
+		while (i < argc - 2)
 			ft_child_process(argv[i++], envp);
+		dup2(fdout, STDOUT_FILENO);
+		close(fdout);
+		ft_execute(argv[argc - 2], envp);
 	}
-
+	else
+		ft_error();
 }
--- bonus/utils_bonus.c
+++ bonus/utils_bonus.c
@@ -12,51 +12,82 @@
 
 #include "pipex_bonus.h"
 
-void	ft_child_process(char *command, char **envp)
-{
-	pid_t	pid;
-
-	pid = fork();
-	//if ()
-}
+// I put two if, for the same infile type, because i the case its the first file
+// i have to modify the stdin 
 int	ft_open_file(char *file, const char *type)
 {
 	int	fd;
 
 	if (!ft_strncmp(type, "heredoc", ft_strlen(type)))
 		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0644);
-	else if (!ft_strncmp(type, "infile", ft_strlen(type)))
-		fd = open(file, O_RDONLY);
 	else if (!ft_strncmp(type, "outfile", ft_strlen(type)))
 		fd = open (file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else
+		fd = open(file, O_RDONLY);
 	if (fd == -1)
-		ft_error();	
+		ft_error();
+	if (!ft_strncmp(type, "infile", ft_strlen(type)))
+	{
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+		return (STDIN_FILENO);
+	}
 	return (fd);
 }
 
 
-void	ft_heredoc(int argc, char **argv)
+void	ft_heredoc(char **argv)
 {
 	int		fd[2];
-	char	*delim;
+	char	*delimiter;
 	char	*content;
 
 	if (pipe(fd) == -1)
 		ft_error();
-	delim = argv[2];
+	delimiter = argv[2];
+	write(1, "> ", 2);
 	content = get_next_line(0);
 	while (content)
 	{
-		if (!ft_strncmp(content, delim, ft_strlen(delim)) 
-			&& content[ft_strlen(delim)] == '\n')
+		if (!ft_strncmp(content, delimiter, ft_strlen(delimiter)) 
+			&& content[ft_strlen(delimiter)] == '\n')
 		{
 			free(content);
 			break;
 		}
+		write(1, "> ", 2);
 		write(fd[1], content, ft_strlen(content));
 		free(content);
 		content = get_next_line(0);
 	}
 	close(fd[1]);
 	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+}
+
+//Deberia de hacer un close del archivo outfile
+void	ft_child_process(char *command, char **envp)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (pipe(fd) == -1)
+		ft_error();
+	pid = fork();
+	if (pid == -1)
+		ft_error();
+	if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		ft_execute(command, envp);
+	}
+	else
+	{
+		wait(NULL);
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+	}
 }
